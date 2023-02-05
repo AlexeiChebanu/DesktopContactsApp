91bd3f9f3c92d82cfde36cce821635ef1ecd6d81
Alexei Chebanu <0alexchebanu@gmail.com>
2023-02-05 15:26:35 +0200

    Add project files.

---
+++ DesktopContactsApp/Classes/Contact.cs
@@ -0,0 +1,27 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DesktopContactsApp.Classes
+{
+    public class Contact
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+/*        [Required]*/
+        public string Name { get; set; }
+/*        [Required]
+        [EmailAddress]*/
+        public string Email { get; set; }
+/*        [Required]
+        [Phone]*/
+        public string Phone { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Email} - {Phone}";
+        }
+    }
+}
