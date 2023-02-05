cee233b0dcde638110c6311d81723dd8f6efb7df
Alexei Chebanu <0alexchebanu@gmail.com>
2023-02-05 19:10:12 +0200

    ListView/Custom User Control

--- DesktopContactsApp/ConsoleManagerWindow.xaml.cs
+++ DesktopContactsApp/ConsoleManagerWindow.xaml.cs
@@ -24,6 +24,9 @@ namespace DesktopContactsApp
         {
             InitializeComponent();
 
+            Owner = Application.Current.MainWindow;
+            WindowStartupLocation = WindowStartupLocation.CenterOwner;
+
             this.contact = contact;
 
             nameTextBox.Text = contact.Name;
